4110187a738e283ff972a264eeb781723c04943c
ricota94 <100703565+ricota94@users.noreply.github.com>
2023-02-13 10:55:55 +0100

    création page chauffeur

---
+++ chauffeur/script.js
@@ -0,0 +1,9 @@
+function transition1(){
+    formulaire2.style.display = "block";
+    formulaire1.style.display = "none";
+}
+
+function transition2(){
+    formulaire1.style.display = "block";
+    formulaire2.style.display = "none";
+}
