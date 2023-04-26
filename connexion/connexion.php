75b3c0b99198cf1ead65f878dceb39fcf73a450a
ricota94 <100703565+ricota94@users.noreply.github.com>
2023-04-26 13:46:15 +0200

    ajout connexion php

---
+++ connexion/connexion.php
@@ -0,0 +1,31 @@
+<?php 
+    session_start(); // Démarrage de la session
+    require_once 'config.php';
+    // On inclut la connexion à la base de données
+
+    if(!empty($_POST['mailco']) && !empty($_POST['mdpco'])) // Si il existe les champs email, password et qu'il sont pas vident
+    {
+        // Patch XSS
+        $email = htmlspecialchars($_POST['mailco']); 
+        $password = htmlspecialchars($_POST['mdpco']);
+        
+        $email = strtolower($email); // email transformé en minuscule
+        
+        //On regarde si l'utilisateur est inscrit dans la table utilisateurs
+        $check = $bdd->prepare('SELECT nom, prenom, mail, mdp FROM user WHERE mail = ?');
+        $check->execute(array($email)); 
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+        // Si > à 0 alors l'utilisateur existe
+        if($row > 0){
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                //$password = hash('sha256', $password);
+                if($data['mdp'] === $password) {
+                    $_SESSION['user'] = $data['prenom'];
+                    header('Location:connexion2.html');
+                }else{ header('Location: ../index.html?login_err=password'); die(); }
+            } else {header('Location:../index.html?login_err=email');}
+        }else{ header('Location: ../index.html?login_err=already'); die(); }
+    }else{ header('Location:../index.html?login_err=empty'); die();}
+?>
